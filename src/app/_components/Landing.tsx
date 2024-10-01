4721ab816ae5b924d990bc393890b1ad61df58ed
Win Cheng <winzamark12@gmail.com>
2024-10-01 12:46:16 -0700

    Landing complete

--- src/app/_components/Landing.tsx
+++ src/app/_components/Landing.tsx
@@ -1,14 +1,55 @@
-import CaMOOraBlack from '@public/name-black.svg';
+import { ContainerScroll } from '@/components/ui/container-scroll-animation';
 import Image from 'next/image';
-import { BackgroundGradientAnimation } from '@/components/ui/background-animation-gradient';
+import CaMOOra from '@public/name-black.svg';
+import CaMOOraDark from '@public/name-white.svg';
+import HomePage from '@public/home-page-light.png';
+import HomePageDark from '@public/home-page-dark.png';
 
 export default function Landing() {
   return (
-    <main className="flex flex-col">
-      <div className="w-screen absolute left-0 top-0 -z-30">
-        <BackgroundGradientAnimation />
-      </div>
-      <Image src={CaMOOraBlack} alt="CaMOOra" width={300} height={300} />
+    <main className="">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <div className="flex flex-col items-center justify-center">
+              <Image
+                className="dark:hidden"
+                src={CaMOOra}
+                alt="CaMOOra"
+                width={300}
+                height={300}
+              />
+              <Image
+                className="hidden dark:flex"
+                src={CaMOOraDark}
+                alt="CaMOOra"
+                width={300}
+                height={300}
+              />
+              <h1 className="text-4xl font-semibold text-black dark:text-white">
+                Student Photoshoot Marketplace <br />
+              </h1>
+            </div>
+          </>
+        }
+      >
+        <Image
+          src={HomePage}
+          alt="hero"
+          height={820}
+          width={1500}
+          className="dark:hidden mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+        <Image
+          src={HomePageDark}
+          alt="hero"
+          height={820}
+          width={1500}
+          className="hidden dark:flex mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Landing from './_components/Landing';
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center">
